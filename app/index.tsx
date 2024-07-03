41c7ae03fcd714a502114c0ee1c8d02be83db066
ElizPN <pankevych.liza@gmail.com>
2024-07-03 16:07:08 +0200

    refactor, move logic to separate component

--- app/index.tsx
+++ app/index.tsx
@@ -1,56 +1,8 @@
 import React from "react";
-import { Text, View, Button, Alert } from "react-native";
-import { postIngridients } from "../services/postIngridients";
+import { View } from "react-native";
+import SubmitIngredients from "../components/SubmitIngredients";
 
 export default function Index() {
-  let ingredients = [
-    "Aqua/Water",
-    "Sodium Laureth Sulfate",
-    "Cocamidopropyl Betaine",
-    "Sodium Lauryl Sulfate",
-    "Glycol Distearate",
-    "Ppg-5-Ceteth-20",
-    "Sodium Chloride",
-    "Amodimethicone",
-    "Parfum/Fragrance",
-    "Sodium Benzoate",
-    "Sodium Hydroxide",
-    "Citric Acid",
-    "Coco-Betaine",
-    "Salicylic Acid",
-    "Guar Hydroxypropyltrimonium Chloride",
-    "Trideceth-6",
-    "Carbomer",
-    "Lactic Acid",
-    "Linalool",
-    "Arginine",
-    "Glutamic Acid",
-    "Benzyl Salicylate",
-    "Benzyl Alcohol",
-    "Serine",
-    "Limonene",
-    "Hydroxypropyltrimonium Hydrolyzed Wheat Protein",
-    "Cetrimonium Chloride",
-    "Amyl Cinnamal",
-    "Coumarin",
-    "Citronellol",
-    "2-Oleamido-1, 3-Octadecanediol",
-    "Alpha-Isomethyl Ionone",
-    "Fumaric Acid",
-    "Phenoxyethanol",
-  ];
-
-  const handleSubmit = async () => {
-    try {
-      const response = await postIngridients(ingredients.join(", "));
-      console.log(response, "response");
-
-      Alert.alert("Success", "Data submitted successfully");
-    } catch (error) {
-      Alert.alert("Error", "There was an error submitting the data");
-    }
-  };
-
   return (
     <View
       style={{
@@ -59,8 +11,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Send ingridients</Text>
-      <Button title="Submit Ingredients" onPress={handleSubmit} />
+      <SubmitIngredients />
     </View>
   );
 }
